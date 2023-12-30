3bf30213b7968b06018672181adb3f7f8bd7b81e
Sean Davie <sean.davie.m@gmail.com>
2023-12-30 17:23:31 -0500

    fix(style): move tw theme colors to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,19 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'my-tan': '#e8dab2',
-      'my-teal': '#4f6d7a',
-      'my-sky-blue': '#c0d6df',
-      'my-egg': '#eaeaea',
-      'my-white': '#ffffff',
-      'my-orange': '#dd6e42'
-    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'my-tan': '#e8dab2',
+        'my-teal': '#4f6d7a',
+        'my-sky-blue': '#c0d6df',
+        'my-egg': '#eaeaea',
+        'my-white': '#ffffff',
+        'my-orange': '#dd6e42'
       },
     },
   },
