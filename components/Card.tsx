f9bfab9dbc4ee50dd4c09762930df62b819df283
Sean Davie <sean.davie.m@gmail.com>
2024-03-16 16:09:00 -0400

    refactor(Card): add ability to add classes

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,9 @@
-export default function Card({ children }: { children: React.ReactNode }) {
+export default function Card({ children, className }: { children: React.ReactNode, className?: string}) {
+
+    const classes = `max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${className}`;
+
     return (
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className={classes}>
             {children}
         </div>
     );
