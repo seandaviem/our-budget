8d1bce37e820d4ddabc531aac36901a493eb5d0e
seandaviem <sean.davie.m@gmail.com>
2024-09-14 21:57:18 -0400

    temp(activities): disable activity pagination

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     description: 'View activities in your current portal.',
 }
 
-export default async function Activities({ searchParams}: { searchParams: { [key: string]: string | string[] | undefined } }) {
+export default async function Activities({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
 
     const dateRangeObj = getDateRangeObj(searchParams);
 
@@ -32,17 +32,17 @@ export default async function Activities({ searchParams}: { searchParams: { [key
                     <Link href="/activities/add-activity" className="btn btn-primary mb-3">
                         Add Activity
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </Link>
-                    <div className="my-5"><DateRangeToggle dateRangeObj={dateRangeObj} /></div> 
-                    <p className="mb-3">Activity:</p> 
-                    { activities.length > 0 ? (
-                        <ActivityTable 
-                            activities={activities} 
-                            categoryOptions={sortedCategoryOptions} 
-                            paymentMethodOptions={paymentMethodOptions} 
-                            showPagintion={true} 
+                    <div className="my-5"><DateRangeToggle dateRangeObj={dateRangeObj} /></div>
+                    <p className="mb-3">Activity:</p>
+                    {activities.length > 0 ? (
+                        <ActivityTable
+                            activities={activities}
+                            categoryOptions={sortedCategoryOptions}
+                            paymentMethodOptions={paymentMethodOptions}
+                            showPagintion={false}
                         />
                     ) : (
                         <p className="text-sm">No activities found.</p>
